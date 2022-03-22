79e36237e3f87d949d5bcf87caabb1e8bfd8e840
ardinaja <ardinfauzi81@gmail.com>
2022-03-22 16:17:15 +0700

    pustaka-booking

---
+++ pustaka-booking/application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh01 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Ardin Fauzi Saleh
+        Saya tingga di daerah kebon jeruk
+        olah raga yang saya sukai adalah
+        Jogging";
+    }
+}
+?>
